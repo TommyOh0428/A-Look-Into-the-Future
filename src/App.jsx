fbfd22026e322a98dad799d48bf958920ac54a39
abdurisaq <ahh8@sfu.ca>
2023-11-23 21:43:11 -0800

    reverted all changes made to index.js and moved changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,20 @@ import WeatherAppWrapper from './component/weatherComponent/weather';
 import Calendar from './component/calendarComponent/calendar';
 import InputField from './component/calendarComponent/input_field';
 import { GoogleAuth } from './component/googleAuth';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient( 
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
+  );
 
 function App() {
+
   return (
+    <SessionContextProvider supabaseClient={supabase}>
     <div className="App">
+      
       <GoogleAuth/>
       
       <Calendar/>
@@ -15,6 +25,7 @@ function App() {
       
       <WeatherAppWrapper />
     </div>
+    </SessionContextProvider>
   );
 }
  
--- src/index.js
+++ src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createClient } from '@supabase/supabase-js';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-
-const supabase = createClient( 
-  "https://nzttrjkhhjujyqixdiuz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56dHRyamtoaGp1anlxaXhkaXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyNjY3MzUsImV4cCI6MjAxNTg0MjczNX0.Hmy7GvbRXZOjW4gjnF20lm0yGrDM0A_-SHsoKClBE_U"
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <SessionContextProvider supabaseClient={supabase}>
+root.render(  
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </SessionContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
