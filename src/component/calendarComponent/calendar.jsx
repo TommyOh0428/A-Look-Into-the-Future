f1e3935d6d0a179966b12368525ab19dcb19ad34
abdurisaq <133296208+abdurisaq@users.noreply.github.com>
2023-11-25 10:34:47 -0800

    removed error of unused useEffect

--- src/component/calendarComponent/calendar.jsx
+++ src/component/calendarComponent/calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { useSession} from '@supabase/auth-helpers-react';
 import EventModal from './eventModal'
 
