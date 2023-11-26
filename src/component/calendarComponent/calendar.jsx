e8008a4bd42df3a445979a8c69cad9d7b8c0d693
kovac04 <uros1201@gmail.com>
2023-11-25 16:10:03 -0800

    closes issue #21 / finished basic delete option

--- src/component/calendarComponent/calendar.jsx
+++ src/component/calendarComponent/calendar.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
+import { useSession} from '@supabase/auth-helpers-react';
+import EventModal from './eventModal'
 
 const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
-
+  const [events, setEvents] = useState([]);
+  const [selectedEventId, setSelectedEventId] = useState(null);
+  console.log(selectedEventId);
   const daysInMonth = (month, year) => {
     return new Date(year, month + 1, 0).getDate();
   };
@@ -16,12 +20,67 @@ const Calendar = () => {
   const currentYear = selectedDate.getFullYear();
   const daysCount = daysInMonth(currentMonth, currentYear);
   const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  const session = useSession();
+  const [showModal, setShowModal] = useState(false);
+
+  const fetchEventsForDay = async (date) => {
+    const startTime = new Date(date);
+    startTime.setHours(0, 0, 0, 0);
+    const endTime = new Date(date);
+    endTime.setHours(23, 59, 59, 999);
+
+    const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${startTime.toISOString()}&timeMax=${endTime.toISOString()}`, {
+      method: "GET",
+      headers: { 'Authorization': 'Bearer ' + session.provider_token,}
+    });
+
+    if (!response.ok) {
+      console.error('Error fetching events');
+      return;
+    }
+
+    const data = await response.json();
+    setEvents(data.items || []);
+  };
 
   const handleDayClick = (day) => {
     const newDate = new Date(currentYear, currentMonth, day);
     setSelectedDate(newDate);
+    if(session && session.provider_token){
+      fetchEventsForDay(newDate);
+      setShowModal(true);
+    }
+    
   };
 
+const handleEventClick = (eventId) => {
+    // You can store this in the state if needed
+    setSelectedEventId(eventId);
+    const event = events.find(event => event.id === eventId);
+    const start = new Date(event.start.dateTime);
+
+  // Format the hours and minutes
+  const startTime = `${start.getHours()}:${start.getMinutes() < 10 ? '0' : ''}${start.getMinutes()}`;
+
+  alert(`Event details:\n\nName: ${event.summary}\n\nEvent starts at: ${startTime}\n`);
+    const shouldDelete = window.confirm('Do you want to delete this event?');
+    if(shouldDelete){
+      deleteEvent(eventId);
+    }
+  };
+
+  async function deleteEvent(eventId) {
+    const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events/${eventId}`, {
+      method: "DELETE",
+      headers: {
+        'Authorization': 'Bearer ' + session.provider_token,  // Access token for Google
+      },
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Failed to delete event: ${response.status}`);
+    } else {alert(`Event deleted`);}
+  }
   const renderCalendar = () => {
     
 
@@ -76,11 +135,16 @@ const Calendar = () => {
       <div className="selected-date">
         Selected Date: {selectedDate.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
       </div>
+      {showModal && (
+        <EventModal
+          events={events}
+          selectedDate={selectedDate}
+          onClose={() => setShowModal(false)}
+          handleEventClick={handleEventClick}
+        />
+      )}
       {renderCalendar()}
-      
     </div>
-    
-   
   );
 };
 
