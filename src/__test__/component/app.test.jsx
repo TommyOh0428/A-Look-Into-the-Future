c5f00c9e170d08e0d120162f64dcef687146a637
abdurisaq <ahh8@sfu.ca>
2023-12-07 10:28:06 -0800

    added 2 new unit tests for inputfield, and signout

--- src/__test__/component/app.test.jsx
+++ src/__test__/component/app.test.jsx
@@ -1,14 +1,19 @@
 import Calendar from '../../component/calendarComponent/calendar';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import { render, screen, fireEvent,waitFor } from '@testing-library/react';
+import { render, screen, fireEvent,waitFor, findByText } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../../App';
+import { async } from 'q';
 
 jest.mock('@supabase/auth-helpers-react', () => ({
   useSession: jest.fn(),
   useSupabaseClient: jest.fn(),
 }));
 describe('Testing UI with login and navigation', () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // Clear all mocked implementations after each test
+  });
+
   test('renders the correct page after sign in', () => {
     // Set up the mock implementations
     useSession.mockImplementation(() => ({
@@ -84,4 +89,40 @@ describe('Testing UI with login and navigation', () => {
     const pageElement = getByText('What would you like to do first?'); 
     expect(pageElement).toBeInTheDocument();
   });
+
+  test('testing that the sign out button signs you out and shows sign in screen', async () => {
+    // Set up the mock implementations
+    useSession.mockImplementation(() => ({
+      user: { email: 'testuser@gmail.com' },
+      provider_token: 'mock_provider_token',
+    }));
+    useSupabaseClient.mockImplementation(() => ({
+      auth: {
+        signInWithOAuth: jest.fn(),
+        signOut: jest.fn(),
+      },
+    }));
+  
+    const { getByText, findByText, rerender } = render(
+      <MemoryRouter initialEntries={['/home']}>
+        <App />
+      </MemoryRouter>
+    ); 
+    
+    const signOutButton = getByText('Sign Out');   
+    fireEvent.click(signOutButton);
+  
+    // Mock the useSession hook to return null and force a re-render of the App component
+    useSession.mockImplementation(() => null);
+    rerender(
+      <MemoryRouter initialEntries={['/home']}>
+        <App />
+      </MemoryRouter>
+    );
+  
+    const signInButton = await waitFor(() => findByText('Sign In With Google'));
+    expect(signInButton).toBeInTheDocument();
+  });
+
+
 });
